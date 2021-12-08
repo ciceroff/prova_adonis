967fe3c31167d1a0640c24f04609e5c88bead730
ciceroff <cicero.barrozo13@gmail.com>
2021-12-08 12:39:05 -0300

    feat: create user's models and seeders

---
+++ database/seeders/User.ts
@@ -0,0 +1,12 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+
+export default class UserSeeder extends BaseSeeder {
+  public async run () {
+    await User.create({
+      name: 'Admin',
+      email: 'admin@loterica.com',
+      password: 'secret'
+    })
+  }
+}
